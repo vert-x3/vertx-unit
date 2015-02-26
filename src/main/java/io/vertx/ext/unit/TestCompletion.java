62f273262bb35236fe2f7f0063d7d1dbce03846a
Julien Viet <julien@julienviet.com>
2015-02-26 21:13:07 +0100

    Missing javadoc

--- src/main/java/io/vertx/ext/unit/TestCompletion.java
+++ src/main/java/io/vertx/ext/unit/TestCompletion.java
@@ -6,13 +6,26 @@ import io.vertx.core.Future;
 import io.vertx.core.Handler;
 
 /**
+ * This object provides callback-ability for the end of a test suite.
+ *
  * @author <a href="mailto:julien@julienviet.com">Julien Viet</a>
  */
 @VertxGen
 public interface TestCompletion {
 
+  /**
+   * Completes the future when all test cases of the test suite passes, otherwise fails it.
+   *
+   * @param future the future to resolve
+   */
   void resolve(Future future);
 
+  /**
+   * Completion handler for the end of the test, the result is successful when all test cases pass otherwise
+   * it is failed.
+   *
+   * @param completionHandler the completion handler
+   */
   void handler(Handler<AsyncResult<Void>> completionHandler);
 
 }
