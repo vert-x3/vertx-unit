f4c4210b7a338987dc0df0be63a99184ccc723de
Clement Escoffier <clement.escoffier@gmail.com>
2015-10-02 19:09:32 +0200

    Replace asciidoc attributes by docgen variables

--- src/main/java/io/vertx/ext/unit/package-info.java
+++ src/main/java/io/vertx/ext/unit/package-info.java
@@ -18,9 +18,9 @@
  * [source,xml,subs="+attributes"]
  * ----
  * <dependency>
- *   <groupId>{maven-groupId}</groupId>
- *   <artifactId>{maven-artifactId}</artifactId>
- *   <version>{maven-version}</version>
+ *   <groupId>${maven.groupId}</groupId>
+ *   <artifactId>${maven.artifactId}</artifactId>
+ *   <version>${maven.version}</version>
  * </dependency>
  * ----
  *
@@ -28,7 +28,7 @@
  *
  * [source,groovy,subs="+attributes"]
  * ----
- * compile {maven-groupId}:{maven-artifactId}:{maven-version}
+ * compile ${maven.groupId}:${maven.artifactId}:${maven.version}
  * ----
  *
  * Vert.x unit can be used in different ways and run anywhere your code runs, it is just a matter of reporting
