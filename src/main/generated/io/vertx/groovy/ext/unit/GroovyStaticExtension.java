691651929c54612ac2d9f1cd433f85c9170f83b2
Julien Viet <julien@julienviet.com>
2016-11-04 14:56:50 +0100

    Regen

--- src/main/generated/io/vertx/groovy/ext/unit/GroovyStaticExtension.java
+++ src/main/generated/io/vertx/groovy/ext/unit/GroovyStaticExtension.java
@@ -1,7 +1,7 @@
 package io.vertx.groovy.ext.unit;
 public class GroovyStaticExtension {
   public static io.vertx.ext.unit.collect.EventBusCollector create(io.vertx.ext.unit.collect.EventBusCollector j_receiver, io.vertx.core.Vertx vertx, java.util.Map<String, Object> options) {
-    return io.vertx.lang.groovy.RetroCompatExtension.wrap(io.vertx.ext.unit.collect.EventBusCollector.create(vertx,
-      options != null ? new io.vertx.ext.unit.report.ReportingOptions(io.vertx.lang.groovy.RetroCompatExtension.toJsonObject(options)) : null));
+    return io.vertx.lang.groovy.ConversionHelper.wrap(io.vertx.ext.unit.collect.EventBusCollector.create(vertx,
+      options != null ? new io.vertx.ext.unit.report.ReportingOptions(io.vertx.lang.groovy.ConversionHelper.toJsonObject(options)) : null));
   }
 }
